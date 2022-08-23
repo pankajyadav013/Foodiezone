9e7d000f7af9165d12370349dd669676ca1a00ae
Pankaj Yadav <78649789+pankajyadav013@users.noreply.github.com>
2022-08-23 22:40:12 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = props => {
   return (
-    <div className="jumbotron">
-        <h1 className="display-1">
+   <div className="jumbotron">
+      <h1 className="display-1">
         <i class="material-icons brand-icon">fastfood</i> Food Recipe
-        </h1>
-        <div class="input-group w-50 mx-auto">
+      </h1>
+      <div class="input-group w-50 mx-auto">
         <input
           type="text"
           class="form-control"
@@ -14,6 +14,11 @@ const Header = props => {
           value={search}
           onChange={onInputChange}
         />
+        <div class="input-group-append">
+          <button className="btn btn-dark" onClick={onSearchClick}>
+            Search Recipe
+          </button>
+        </div>
       </div>
       
     </div>
